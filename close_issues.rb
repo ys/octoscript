df69fc88bc11f35484db7981966d2d9e702baa78
Yannick Schutz <yannick.schutz@gmail.com>
2013-07-29 19:29:46 +0200

    Create close_issues.rb

---
+++ close_issues.rb
@@ -0,0 +1,20 @@
+require 'octokit'
+
+puts 'repo'
+repo = gets
+puts 'search term for issues'
+search_term = gets
+puts 'username'
+username = gets
+puts 'password'
+password = gets
+
+client = Octokit::Client.new(:login => username, :password => password)
+
+do
+  issues = client.list_issues(repo, state: :open).reject{ |issue| !(issue.title =~ /#{search_term}/)
+  issues.each do |issue|
+    client.close_issue(repo, issue.number)
+  end
+  repo = client.list_issues(repo, state: :open)
+while issues.any?
